0c052b3f03f374c4453667de286f650580b003f2
bsli <18722214517@163.com>
2022-06-15 10:24:20 +0800

    注释首页展示内容 修改登录标题和企业信息

--- ruoyi-ui/src/views/index.vue
+++ ruoyi-ui/src/views/index.vue
@@ -3,7 +3,7 @@
     <el-row :gutter="20">
       <el-col :sm="24" :lg="24">
         <blockquote class="text-warning" style="font-size: 14px">
-          领取阿里云通用云产品1888优惠券
+          <!-- 领取阿里云通用云产品1888优惠券
           <br />
           <el-link
             href="https://www.aliyun.com/minisite/goods?userCode=brki8iof"
@@ -31,7 +31,7 @@
           ><br />
           <h4 class="text-danger">
             云产品通用红包，可叠加官网常规优惠使用。(仅限新用户)
-          </h4>
+          </h4> -->
         </blockquote>
 
         <hr />
@@ -39,7 +39,7 @@
     </el-row>
     <el-row :gutter="20">
       <el-col :sm="24" :lg="12" style="padding-left: 20px">
-        <h2>若依后台管理框架</h2>
+        <!-- <h2>若依后台管理框架</h2>
         <p>
           一直想做一款后台管理系统，看了很多优秀的开源项目但是发现没有合适自己的。于是利用空闲休息时间开始自己写一套后台系统。如此有了若依管理系统，她可以用于所有的Web应用程序，如网站管理后台，网站会员中心，CMS，CRM，OA等等，当然，您也可以对她进行深度定制，以做出更强系统。所有前端后台代码封装过后十分精简易上手，出错概率低。同时支持移动客户端访问。系统会陆续更新一些实用功能。
         </p>
@@ -65,18 +65,18 @@
             @click="goTarget('http://ruoyi.vip')"
             >访问主页</el-button
           >
-        </p>
+        </p> -->
       </el-col>
 
       <el-col :sm="24" :lg="12" style="padding-left: 50px">
         <el-row>
           <el-col :span="12">
-            <h2>技术选型</h2>
+            <!-- <h2>技术选型</h2> -->
           </el-col>
         </el-row>
         <el-row>
           <el-col :span="6">
-            <h4>后端技术</h4>
+            <!-- <h4>后端技术</h4>
             <ul>
               <li>SpringBoot</li>
               <li>SpringCloud</li>
@@ -85,10 +85,10 @@
               <li>Seata</li>
               <li>Minio</li>
               <li>...</li>
-            </ul>
+            </ul> -->
           </el-col>
           <el-col :span="6">
-            <h4>前端技术</h4>
+            <!-- <h4>前端技术</h4>
             <ul>
               <li>Vue</li>
               <li>Vuex</li>
@@ -97,7 +97,7 @@
               <li>Echarts</li>
               <li>Quill</li>
               <li>...</li>
-            </ul>
+            </ul> -->
           </el-col>
         </el-row>
       </el-col>
@@ -107,10 +107,10 @@
       <el-col :xs="24" :sm="24" :md="12" :lg="8">
         <el-card class="update-log">
           <div slot="header" class="clearfix">
-            <span>联系信息</span>
+            <!-- <span>联系信息</span> -->
           </div>
           <div class="body">
-            <p>
+            <!-- <p>
               <i class="el-icon-s-promotion"></i> 官网：<el-link
                 href="http://www.ruoyi.vip"
                 target="_blank"
@@ -136,16 +136,16 @@
                 class="支付宝信息"
                 >/ *若依</a
               >
-            </p>
+            </p> -->
           </div>
         </el-card>
       </el-col>
       <el-col :xs="24" :sm="24" :md="12" :lg="8">
         <el-card class="update-log">
           <div slot="header" class="clearfix">
-            <span>更新日志</span>
+            <!-- <span>更新日志</span> -->
           </div>
-          <el-collapse accordion>
+          <!-- <el-collapse accordion>
             <el-collapse-item title="v3.5.0 - 2022-04-11">
               <ol>
                 <li>开启TopNav没有子菜单情况隐藏侧边栏</li>
@@ -686,23 +686,23 @@
                 <li>若依微服务系统正式发布</li>
               </ol>
             </el-collapse-item>
-          </el-collapse>
+          </el-collapse> -->
         </el-card>
       </el-col>
       <el-col :xs="24" :sm="24" :md="12" :lg="8">
         <el-card class="update-log">
           <div slot="header" class="clearfix">
-            <span>捐赠支持</span>
+            <!-- <span>捐赠支持</span> -->
           </div>
           <div class="body">
-            <img
+            <!-- <img
               src="https://oscimg.oschina.net/oscnet/up-d6695f82666e5018f715c41cb7ee60d3b73.png"
               alt="donate"
               width="100%"
             />
             <span style="display: inline-block; height: 30px; line-height: 30px"
               >你可以请作者喝杯咖啡表示鼓励</span
-            >
+            > -->
           </div>
         </el-card>
       </el-col>
