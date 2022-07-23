7eeb585debd07091f1d632b857c77bf9c21d90dc
wwj <398762487@qq.com>
2022-07-23 14:07:43 +0800

    工商管理增加文件上传功能0723

--- ruoyi-modules/ruoyi-company/src/main/java/com/ruoyi/company/controller/WordExportController.java
+++ ruoyi-modules/ruoyi-company/src/main/java/com/ruoyi/company/controller/WordExportController.java
@@ -1,15 +1,12 @@
 package com.ruoyi.company.controller;
 
-import com.google.code.appengine.awt.Color;
-import com.lowagie.text.Font;
+
 import com.ruoyi.company.config.ConfigProps;
 import com.ruoyi.company.domain.SelfEmployed;
 import com.ruoyi.company.service.ISelfEmployedService;
-import fr.opensagres.poi.xwpf.converter.pdf.PdfConverter;
-import fr.opensagres.poi.xwpf.converter.pdf.PdfOptions;
-import fr.opensagres.xdocreport.itext.extension.font.IFontProvider;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
+import org.apache.commons.io.IOUtils;
 import org.apache.poi.xwpf.usermodel.*;
 import org.openxmlformats.schemas.wordprocessingml.x2006.main.CTTc;
 import org.openxmlformats.schemas.wordprocessingml.x2006.main.CTTcPr;
@@ -20,12 +17,9 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.annotation.Resource;
-import javax.servlet.ServletOutputStream;
-import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.*;
 import java.text.SimpleDateFormat;
-import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
 import java.util.UUID;
@@ -42,21 +36,38 @@ public class WordExportController {
 
     @GetMapping("/showTxt")
     @ApiOperation("查看文件")
-    public List<String> showTxt(String imgPath, HttpServletResponse resp) throws IOException {
-//        String imgPath = req.getParameter("imgPath");
-//        FileInputStream txt = new FileInputStream(configProps.getName() + imgPath);
-        File file=new File(configProps.getName() + imgPath);
-        ArrayList<String> list = new ArrayList<>();
-        try (BufferedReader reader =
-                     new BufferedReader(new InputStreamReader(new FileInputStream(file)))) {
-            String line;
-            while ((line = reader.readLine()) != null) {
-                list.add(line);
-            }
+    public void showTxt(String imgPath, HttpServletResponse response) throws IOException {
+        // 设置编码
+//        response.setCharacterEncoding("UTF-8");
+//        response.setContentType("application/vnd.openxmlformats-officedocument.wordprocessingml.document");
+//        response.addHeader("Content-Disposition", "attachment;filename="+ URLEncoder.encode(imgPath, "UTF-8"));
+//        String path = configProps.getName() + imgPath;
+        String path ="/data/img/" + imgPath;
+        File file=new File(path);
+        System.out.println("绝对路径=="+file.getAbsolutePath());
+        try {
+            FileInputStream in = new FileInputStream(file);
+//            response.setHeader("Content-Type", "application/pdf");
+            OutputStream out = response.getOutputStream();
+            IOUtils.write(IOUtils.toByteArray(in), out);
+//            FileCopyUtils.copy(in, response.getOutputStream());
+        } catch (FileNotFoundException e) {
+            System.out.println("文件不存在");
         } catch (IOException e) {
-            throw new RuntimeException("Error parsing file.", e);
+            e.printStackTrace();
         }
-        return list;
+//        File file=new File(configProps.getName() + imgPath);
+//        System.out.println("绝对路径=="+file.getAbsolutePath());
+//        ArrayList<String> list = new ArrayList<>();
+//        try (BufferedReader reader =
+//                     new BufferedReader(new InputStreamReader(new FileInputStream(file.getAbsolutePath())))) {
+//            String line;
+//            while ((line = reader.readLine()) != null) {
+//                list.add(line);
+//            }
+//        } catch (IOException e) {
+//            throw new RuntimeException("Error parsing file.", e);
+//        }
     }
 
     private void setTableText(XWPFDocument docxDocument) {
@@ -151,153 +162,6 @@ public class WordExportController {
 
         //给表格填充文本
         setTableText(docxDocument);
-
-//        //创建一个表格，并指定宽度
-//        XWPFTable table = doc.createTable(4, 4);
-//        TableTools.widthTable(table, MiniTableRenderData.WIDTH_A4_FULL, 4);
-//        //设置第0行数据
-//        List<XWPFTableCell> row0 = table.getRow(0).getTableCells();
-//        row0.get(0).setText("xxxx"); //为第0行第0列设置内容
-//        row0.get(0).setWidth("200");
-//        row0.get(1).setText("aaaa");
-//        row0.get(2).setText("bbbb");
-//        row0.get(3).setText("cccc");
-
-
-//        //添加标题
-//        XWPFParagraph titleParagraph = document.createParagraph();
-//        //设置段落居中
-//        titleParagraph.setAlignment(ParagraphAlignment.CENTER);
-//
-//        XWPFRun titleParagraphRun = titleParagraph.createRun();
-//        titleParagraphRun.setText("Java PoI 测试");
-//        titleParagraphRun.setColor("000000");
-//        titleParagraphRun.setFontSize(20);
-
-
-//        //段落
-//        XWPFParagraph firstParagraph = document.createParagraph();
-//        XWPFRun run = firstParagraph.createRun();
-//        run.setText("Java POI 生成word文件。");
-//        run.setColor("696969");
-//        run.setFontSize(16);
-//
-//        //设置段落背景颜色
-//        CTShd cTShd = run.getCTR().addNewRPr().addNewShd();
-//        cTShd.setVal(STShd.CLEAR);
-//        cTShd.setFill("97FFFF");
-//
-//
-//        //换行
-//        XWPFParagraph paragraph1 = document.createParagraph();
-//        XWPFRun paragraphRun1 = paragraph1.createRun();
-//        paragraphRun1.setText("\r");
-//
-//
-//        //基本信息表格
-//        XWPFTable infoTable = document.createTable();
-//        //去表格边框
-//        infoTable.getCTTbl().getTblPr().unsetTblBorders();
-//
-//
-//        //列宽自动分割
-//        CTTblWidth infoTableWidth = infoTable.getCTTbl().addNewTblPr().addNewTblW();
-//        infoTableWidth.setType(STTblWidth.DXA);
-//        infoTableWidth.setW(BigInteger.valueOf(9072));
-//
-//
-//        //表格第一行
-//        XWPFTableRow infoTableRowOne = infoTable.getRow(0);
-//        infoTableRowOne.getCell(0).setText("职位");
-//        infoTableRowOne.addNewTableCell().setText(": Java 开发工程师");
-//
-//        //表格第二行
-//        XWPFTableRow infoTableRowTwo = infoTable.createRow();
-//        infoTableRowTwo.getCell(0).setText("姓名");
-//        infoTableRowTwo.getCell(1).setText(": seawater");
-//
-//        //表格第三行
-//        XWPFTableRow infoTableRowThree = infoTable.createRow();
-//        infoTableRowThree.getCell(0).setText("生日");
-//        infoTableRowThree.getCell(1).setText(": xxx-xx-xx");
-//
-//        //表格第四行
-//        XWPFTableRow infoTableRowFour = infoTable.createRow();
-//        infoTableRowFour.getCell(0).setText("性别");
-//        infoTableRowFour.getCell(1).setText(": 男");
-//
-//        //表格第五行
-//        XWPFTableRow infoTableRowFive = infoTable.createRow();
-//        infoTableRowFive.getCell(0).setText("现居地");
-//        infoTableRowFive.getCell(1).setText(": xx");
-//
-//
-//        //两个表格之间加个换行
-//        XWPFParagraph paragraph = document.createParagraph();
-//        XWPFRun paragraphRun = paragraph.createRun();
-//        paragraphRun.setText("\r");
-//
-//
-//
-//        //工作经历表格
-//        XWPFTable ComTable = document.createTable();
-//
-//
-//        //列宽自动分割
-//        CTTblWidth comTableWidth = ComTable.getCTTbl().addNewTblPr().addNewTblW();
-//        comTableWidth.setType(STTblWidth.DXA);
-//        comTableWidth.setW(BigInteger.valueOf(9072));
-//
-//        //表格第一行
-//        XWPFTableRow comTableRowOne = ComTable.getRow(0);
-//        comTableRowOne.getCell(0).setText("开始时间");
-//        comTableRowOne.addNewTableCell().setText("结束时间");
-//        comTableRowOne.addNewTableCell().setText("公司名称");
-//        comTableRowOne.addNewTableCell().setText("title");
-//
-//        //表格第二行
-//        XWPFTableRow comTableRowTwo = ComTable.createRow();
-//        comTableRowTwo.getCell(0).setText("2016-09-06");
-//        comTableRowTwo.getCell(1).setText("至今");
-//        comTableRowTwo.getCell(2).setText("seawater");
-//        comTableRowTwo.getCell(3).setText("Java开发工程师");
-//
-//        //表格第三行
-//        XWPFTableRow comTableRowThree = ComTable.createRow();
-//        comTableRowThree.getCell(0).setText("2016-09-06");
-//        comTableRowThree.getCell(1).setText("至今");
-//        comTableRowThree.getCell(2).setText("seawater");
-//        comTableRowThree.getCell(3).setText("Java开发工程师");
-//
-//
-//        CTSectPr sectPr = document.getDocument().getBody().addNewSectPr();
-//        XWPFHeaderFooterPolicy policy = new XWPFHeaderFooterPolicy(document, sectPr);
-//
-//        //添加页眉
-//        CTP ctpHeader = CTP.Factory.newInstance();
-//        CTR ctrHeader = ctpHeader.addNewR();
-//        CTText ctHeader = ctrHeader.addNewT();
-//        String headerText = "Java POI create MS word file.";
-//        ctHeader.setStringValue(headerText);
-//        XWPFParagraph headerParagraph = new XWPFParagraph(ctpHeader, document);
-//        //设置为右对齐
-//        headerParagraph.setAlignment(ParagraphAlignment.RIGHT);
-//        XWPFParagraph[] parsHeader = new XWPFParagraph[1];
-//        parsHeader[0] = headerParagraph;
-//        policy.createHeader(XWPFHeaderFooterPolicy.DEFAULT, parsHeader);
-//
-//
-//        //添加页脚
-//        CTP ctpFooter = CTP.Factory.newInstance();
-//        CTR ctrFooter = ctpFooter.addNewR();
-//        CTText ctFooter = ctrFooter.addNewT();
-//        String footerText = "http://blog.csdn.net/zhouseawater";
-//        ctFooter.setStringValue(footerText);
-//        XWPFParagraph footerParagraph = new XWPFParagraph(ctpFooter, document);
-//        headerParagraph.setAlignment(ParagraphAlignment.CENTER);
-//        XWPFParagraph[] parsFooter = new XWPFParagraph[1];
-//        parsFooter[0] = footerParagraph;
-//        policy.createFooter(XWPFHeaderFooterPolicy.DEFAULT, parsFooter);
 
         //写到本地
         docxDocument.write(out);
