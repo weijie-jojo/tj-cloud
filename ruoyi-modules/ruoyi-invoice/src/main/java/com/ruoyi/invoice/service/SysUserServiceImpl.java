c92e98900631953a3e4a856f76eed79d5115667e
wwj <398762487@qq.com>
2022-08-22 13:03:30 +0800

    个体户中查询员工信息，单据管理查询用户 改成redis查询

--- ruoyi-modules/ruoyi-invoice/src/main/java/com/ruoyi/invoice/service/SysUserServiceImpl.java
+++ ruoyi-modules/ruoyi-invoice/src/main/java/com/ruoyi/invoice/service/SysUserServiceImpl.java
@@ -1,22 +1,29 @@
 package com.ruoyi.invoice.service;
 
 import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
+import com.ruoyi.common.core.constant.CacheConstants;
+import com.ruoyi.common.redis.service.RedisService;
 import com.ruoyi.invoice.mapper.SysUserMapper;
 import com.ruoyi.invoice.pojo.SysUser;
 import com.ruoyi.invoice.vo.SysUserVo;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import javax.annotation.Resource;
 import java.util.List;
+import java.util.concurrent.TimeUnit;
 
 
 @Service
 @Transactional(rollbackFor = Exception.class)
 public class SysUserServiceImpl implements SysUserService {
+    private final static long expireTime = CacheConstants.EXPIRATION;
+
     @Resource
     private SysUserMapper sysUserMapper;
-
+    @Autowired
+    private RedisService redisService;
     @Override
     public SysUser selectByCompany(Integer userId) {
         QueryWrapper<SysUser> queryWrapper=new QueryWrapper<>();
@@ -43,6 +50,10 @@ public class SysUserServiceImpl implements SysUserService {
     @Override
     public List<SysUser> selectAll() {
         QueryWrapper<SysUser> queryWrapper=new QueryWrapper<>();
-        return sysUserMapper.selectList(queryWrapper);
+        if (redisService.getCacheObject("users")==null){
+            List<SysUser> list = sysUserMapper.selectList(queryWrapper);
+            redisService.setCacheObject("users", list, expireTime, TimeUnit.MINUTES);
+        }
+        return redisService.getCacheObject("users");
     }
 }
