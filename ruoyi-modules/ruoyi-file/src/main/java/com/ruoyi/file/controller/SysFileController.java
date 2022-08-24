2c097688c3b4d20d94979180ce0a4429c49bb296
wwj <398762487@qq.com>
2022-08-24 14:09:33 +0800

    增加文件接口

--- ruoyi-modules/ruoyi-file/src/main/java/com/ruoyi/file/controller/SysFileController.java
+++ ruoyi-modules/ruoyi-file/src/main/java/com/ruoyi/file/controller/SysFileController.java
@@ -1,15 +1,30 @@
 package com.ruoyi.file.controller;
 
+import com.ruoyi.file.config.ConfigProps;
+import com.ruoyi.file.dto.DataDto;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import org.apache.commons.io.IOUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 import com.ruoyi.common.core.domain.R;
 import com.ruoyi.common.core.utils.file.FileUtils;
 import com.ruoyi.file.service.ISysFileService;
 import com.ruoyi.system.api.domain.SysFile;
+
+import javax.annotation.Resource;
+import javax.imageio.ImageIO;
+import javax.servlet.ServletOutputStream;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.awt.image.BufferedImage;
+import java.io.*;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.UUID;
 
 /**
  * 文件请求处理
@@ -17,13 +32,16 @@ import com.ruoyi.system.api.domain.SysFile;
  * @author ruoyi
  */
 @RestController
+@RequestMapping("/api/files")
+@Api(tags = "下载上传")
 public class SysFileController
 {
     private static final Logger log = LoggerFactory.getLogger(SysFileController.class);
 
     @Autowired
     private ISysFileService sysFileService;
-
+    @Autowired
+    private ConfigProps configProps;
     /**
      * 文件上传请求
      */
@@ -45,4 +63,46 @@ public class SysFileController
             return R.fail(e.getMessage());
         }
     }
+
+    @PostMapping(value ="/doUpload")
+    @ApiOperation("上传文件")
+    public DataDto<Integer> postFile(@RequestParam(value="file",required=true) MultipartFile file) throws IOException {
+        Date date = new Date();
+        SimpleDateFormat sdf = new SimpleDateFormat("yyyyMMddhhss");
+        String nowDate = sdf.format(date);
+        String guid= UUID.randomUUID().toString();
+        String fileName=nowDate+guid+file.getOriginalFilename();
+        System.out.println("fileName"+fileName);
+        org.apache.commons.io.FileUtils.copyToFile(file.getInputStream(),
+                new File(configProps.getName() + fileName));
+        DataDto dto = new DataDto();
+        dto.success("文件上传成功",fileName,configProps.getName()+fileName);
+        return dto;
+    }
+    @GetMapping("/showImg")
+    @ApiOperation("查看图片")
+    public void showImg(HttpServletRequest req, HttpServletResponse resp) throws IOException {
+        String imgPath = req.getParameter("imgPath");
+        BufferedImage image = ImageIO.read(new FileInputStream("/data/img/" + imgPath));
+        ServletOutputStream out = resp.getOutputStream();
+        ImageIO.write(image, "png", out);
+    }
+
+    @GetMapping("/showTxt")
+    @ApiOperation("查看文件")
+    public void showTxt(String imgPath, HttpServletResponse response) throws IOException {
+        String path ="/data/img/" + imgPath;
+        File file=new File(path);
+        System.out.println("绝对路径=="+file.getAbsolutePath());
+        try {
+            FileInputStream in = new FileInputStream(file);
+            OutputStream out = response.getOutputStream();
+            IOUtils.write(IOUtils.toByteArray(in), out);
+        } catch (FileNotFoundException e) {
+            System.out.println("文件不存在");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
 }
