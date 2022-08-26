f1fe3e8d4ea03e2b5c8575423e99e67a05ecb422
keshiwang <1838537073@qq.com>
2022-08-26 15:23:47 +0800

    修改

---
+++ ruoyi-ui/src/views/projectlist/examineMeans/aduitMeans.vue
@@ -0,0 +1,284 @@
+<template>
+    <div>
+        <el-form ref="elForm" :model="formData"  size="medium" label-width="140px">
+
+           
+            <el-row type="flex" class="row-bg rowCss combottom" style="padding-top: 20px;" justify="space-around">
+                <el-col :span="9">
+                    <el-form-item class="comright" label="项目编号" :required="true">
+                        <el-input v-model="formData.projectCode" :readonly="true"></el-input>
+                    </el-form-item>
+
+                    <el-form-item class="comright" label="项目名称" :required="true">
+                        <el-input v-model="formData.projectName" :readonly="true"></el-input>
+                    </el-form-item>
+                </el-col>
+
+                <el-col :span="9">
+
+                    <el-form-item class="comright" label="项目时间" :required="true">
+                        <el-input v-model="formData.createTime" :readonly="true"></el-input>
+                    </el-form-item>
+                    <el-form-item class="comright" label="项目金额" :required="true">
+                        <el-input :readonly="true" type="number" style="width:100%" v-model="formData.projectTotalAmount" 
+                            :step="0.01" :min="0"
+                             oninput = 'value = (value.match(/^[0-9]+(\.[0-9]{0,2})?/g) ?? [""])[0]'
+                            >
+                            <template slot="append">
+                              元
+                            </template>
+                        </el-input>
+                    </el-form-item>
+                </el-col>
+            </el-row>
+           <el-row type="flex" class="row-bg " justify="space-around">
+                <el-col :span="9">
+                  <el-form-item class="comright" label="甲方" :required="true">
+                        <el-input v-model="formData.purchCompany" :readonly="true"></el-input>
+                    </el-form-item>
+                   
+                     <el-form-item class="comright" label="项目验收资料" :required="true">
+                          <uploadSmall v-if="fileName.length>0" @getfileName="getfileNameS" :fileName="isNone" :fileNameOld="fileName" :isDetail="isDetail"></uploadSmall>
+                    </el-form-item>
+                </el-col>
+
+                <el-col :span="9">
+                 <el-form-item class="comright" label="乙方" prop="projectOwner">
+                        <el-input  v-model="formData.selfName" :readonly="true"></el-input>
+                    </el-form-item>
+                  </el-col>
+            </el-row>
+              
+             <el-row type="flex" class="row-bg" justify="space-around">
+          <el-col :span="21">
+            <el-form-item class="comright" style="padding-right: 4.2%;margin-left: -7%;">
+              <el-radio v-model="isokradioS" label="1"> 通过</el-radio>
+           </el-form-item>
+        </el-col>
+
+      </el-row>   
+       <el-row type="flex" class="row-bg" justify="space-around">
+        <el-col :span="21">
+          <el-form-item class="comright" style="padding-right: 4.2%;margin-left: -7%;">
+            <div style="display: flex; align-items: center;justify-content: flex-start;">
+              <el-radio v-model="isokradioS" label="2">不通过 </el-radio>
+              <el-input type="textarea" placeholder="请输入不通过说明" v-model="remark" :disabled="isokradioS == 1"></el-input>
+            </div>
+
+
+          </el-form-item>
+        </el-col>
+
+      </el-row>
+            <el-row type="flex" class="row-bg " justify="space-around">
+                <el-col :span="8"></el-col>
+                <el-col :span='8' class="flexs">
+                
+                     <el-button type="danger" @click="resetForm">关闭</el-button>
+                     <el-button v-if="isokradioS == 2" type="primary" @click="submitForm(2)">提交</el-button>
+                     <el-button v-else type="primary" @click="submitForm(1)">提交</el-button>
+                </el-col>
+                <el-col :span="8"></el-col>
+            </el-row>
+      </el-form>
+     </div>
+</template>
+<script>
+import uploadSmall from '@/components/douploads/uploadSmall'
+import {edit,check} from "@/api/project/list"
+import { getInfo } from '@/api/login'
+export default {
+    name:'AduitMeans',
+    components: { uploadSmall },
+    data() {
+        return {
+           projectStatusNew:0,
+            remark:'',
+            isNone:[],
+            isDetail:'1',
+            isokradioS:'1',
+            fileName: [],
+            userinfo:{},
+            formData:{},
+            baseImgPath: "/eladmin/api/files/showTxt?imgPath=",
+           };
+    },
+    computed: {},
+    mounted() {
+        this.formData=this.$cache.local.getJSON("projectListNews");
+        this.formData.fileName2=JSON.parse(this.formData.fileName2);
+        for(let i in this.formData.fileName2){
+           this.fileName.push({
+            name:this.formData.fileName2[i],
+            url:this.baseImgPath+this.formData.fileName2[i]
+           })
+        }
+    },
+    methods: {
+      check(resmsg) {
+        getInfo().then(res => {
+            this.userinfo=res.user;
+             let parms = {
+              "checkReasult": resmsg,
+              "checkUser": this.userinfo.userName,
+              'phonenumber': this.userinfo.phonenumber,
+              "projectCode": this.formData.projectCode,
+              "projectType": "4",
+            };
+            check(parms).then(res => {
+                console.log('验收审核完成');
+            }).catch(error => {
+
+            });
+          })
+       
+       },
+      getfileNameS(){
+
+      },
+      submitForm(type) {
+      this.$refs['elForm'].validate(valid => {
+        // TODO 提交表单
+        if (valid) {
+          let parms;
+          this.projectStatusNew=0;
+          if(this.formData.projectContractStatus==2 || this.formData.projectDutypaidStatus==2){
+              this.projectStatusNew=1;
+          }
+          if (type == 1) {
+            parms = {
+              projectId: this.formData.projectId,
+              projectAcceptanceStatus:type,
+              projectStatus:this.projectStatusNew
+            };
+          } else {
+            parms = {
+              projectId: this.formData.projectId,
+              checkContent: this.remark,
+              projectAcceptanceStatus:type,
+              projectStatus:1,
+            };
+          }
+           edit(parms).then((res) => {
+            if (res != undefined) {
+              if (res.code === 200) {
+                 this.$nextTick(function () {
+                 
+                    let resmsg = '';
+                    if (type == 1) {
+                      resmsg = '验收审核完成';
+                      this.check('验收审核完成');
+                    } else {
+                      this.check('验收审核完成不通过。'+'原因:'+this.remark);
+                      resmsg = '验收审核完成';
+                    }
+
+                   let obj = {
+                      title: '验收审核',
+                      backUrl: this.$cache.local.getJSON('aduitProjectBack').backurl,
+                      resmsg: resmsg,
+                      backName:this.$cache.local.getJSON('aduitProjectBack').name
+
+                    }
+                    this.$cache.local.setJSON('successProject', obj);
+                    this.$tab.closeOpenPage({ path: "/projectlist/success" });
+                
+                });
+
+              } else {
+                this.$modal.msgError(res.msg);
+                this.$tab.closeOpenPage({ path: this.$cache.local.getJSON('aduitProjectBack').backurl });
+              }
+
+            }
+         });
+
+        } else {
+          this.$alert('请正确填写', '系统提示', {
+                        confirmButtonText: '确定',
+                       
+                        type: 'warning'
+                        });
+        }
+      })
+
+    },
+   
+      //返回
+       resetForm(){
+         this.$tab.closeOpenPage({path:this.$cache.local.getJSON('aduitProjectBack').backurl})
+       },
+    
+        handleChange(val) {
+            console.log(val);
+        },
+       
+        
+    },
+};
+</script>
+<style rel="stylesheet/scss" lang="scss" scoped>
+.rowCss {
+    margin-top: 10px;
+}
+
+// 改变input框字体颜色
+::v-deep .is-disabled .el-input__inner {
+    background-color: transparent !important;
+    color: black;
+}
+
+.paddingbg-s {
+    padding-top: 15px;
+}
+
+.footers {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+}
+
+.rowCss {
+    margin-top: 10px;
+}
+
+.comright {
+    padding-right: 10%;
+}
+
+.combottom {
+    margin-bottom: 10px;
+
+}
+
+.flexs {
+    display: flex;
+    justify-content: center;
+
+}
+
+.bankno {
+
+    letter-spacing: 2px;
+
+    font-size: 20px;
+
+    color: blue;
+}
+
+::v-deep .el-input.is-disabled .el-input__inner{
+   background-color: rgba(255, 255, 255, 1.5) !important;
+   color: black  !important;
+   border-color: rgba(135,206,250,0.7) !important;
+}
+::v-deep .el-input-group__append{
+   background-color: rgba(255, 255, 255, 1.5) !important;
+   color: black  !important;
+   border-color: rgba(135,206,250,0.7) !important;
+}
+
+// ::v-deep .el-tabs__nav-scroll {
+//   width: 50% !important;
+//   margin: 0 auto !important;
+// }
+</style>
