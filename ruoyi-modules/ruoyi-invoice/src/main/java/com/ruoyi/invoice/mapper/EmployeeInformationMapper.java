d9bcb84a0703ce369c5a63b59ecbea1f9692a647
wwj <398762487@qq.com>
2022-08-31 18:15:23 +0800

    查询岗位信息接口改成去user表获取

--- ruoyi-modules/ruoyi-invoice/src/main/java/com/ruoyi/invoice/mapper/EmployeeInformationMapper.java
+++ ruoyi-modules/ruoyi-invoice/src/main/java/com/ruoyi/invoice/mapper/EmployeeInformationMapper.java
@@ -10,6 +10,6 @@ import org.apache.ibatis.annotations.Select;
 import java.util.List;
 
 public interface EmployeeInformationMapper extends MPJBaseMapper<EmployeeInformation> {
-    @Select("SELECT B.post_name,t.user_id, t.dept_id, t.department, t.Employee_number, t.username, t.nick_name, t.id_no, t.gender, t.post_id, t.phone, t.email, t.enabled, t.pay_check, t.pay_check_bank, t.account_card, t.account_card_bank, t.create_by, t.update_by, t.create_time, t.update_time FROM employee_information t JOIN sys_user_post A on t.user_id=A.user_id  JOIN sys_post B on A.post_id=B.post_id WHERE  t.user_id=#{userId}")
+    @Select("SELECT B.post_name, t.user_id, t.dept_id, t.user_name, t.nick_name, t.user_type, t.email, t.phonenumber, t.sex, t.avatar, t.password, t.status, t.del_flag, t.login_ip, t.login_date, t.create_by, t.create_time, t.update_by, t.update_time, t.remark, t.Employee_number, t.id_no, t.post_id, t.is_admin, t.pay_check, t.pay_check_bank, t.account_card_bank, t.pwd_reset_time FROM sys_user t JOIN sys_user_post A on t.user_id=A.user_id  JOIN sys_post B on A.post_id=B.post_id WHERE  t.user_id=#{userId}")
     EmployeeInformation getPost(@Param("userId") Integer userId);
 }
